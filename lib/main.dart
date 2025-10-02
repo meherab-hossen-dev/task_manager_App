a3c6988ec05c4551bbb8b372d339cdb4198c8b52
unknown <mhnnishat0102@gmail.com>
2025-10-02 19:00:43 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'myApp.dart';
+
+void maina(){
+
+  runApp(App());
+}
---
+++ lib/myApp.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      title:'Task manager App',
+      home: Home()
+    );
+  }
+}
